ab50ff9629aafc8907fdd850f2310b4be7d8ad38
alanfogel <alanjfogel@gmail.com>
2024-09-27 17:19:42 -0600

    Cenetered Camera on Origin, set default animation to Idle, created palyer script.

--- a2/player.gd
+++ a2/player.gd
@@ -1,15 +1,11 @@
-extends AnimatedSprite2D
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	play("Warping");
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# if some_condition:
-	# 	play("some_animation")
-	# else:
-	# 	play("idle")
 	pass
