edd17e646054ea958461150a41a422ae953dcbfd
alanfogel <alanjfogel@gmail.com>
2024-09-27 17:36:41 -0600

    Added Left and Right rotation with key presses, and both animations

--- a2/player.gd
+++ a2/player.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export var turn_speed := 3.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _physics_process(delta: float) -> void:
+	var animated_sprite = $AnimatedSprite2D
+
+	if animated_sprite != null:
+		if Input.is_action_pressed("ui_left"):
+			rotate(-turn_speed * delta)
+			animated_sprite.play("RotateLeft")
+		elif Input.is_action_pressed("ui_right"):
+			rotate(turn_speed * delta)
+			animated_sprite.play("RotateRight")
+		elif Input.is_action_pressed("ui_accept"):
+			animated_sprite.play("FireBullet")
+		else:
+			animated_sprite.play("Idle")
