f426bae2904f22190a2a4e65f7e2c6a3fb814020
alanfogel <alanjfogel@gmail.com>
2024-09-27 18:04:35 -0600

    Added bullet fireing animation, and pushing player backwards when fired

--- a2/player.gd
+++ a2/player.gd
@@ -1,6 +1,8 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var turn_speed := 3.0
+@export var recoil_force := 1.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +10,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
@@ -22,6 +24,14 @@ func _physics_process(delta: float) -> void:
 			rotate(turn_speed * delta)
 			animated_sprite.play("RotateRight")
 		elif Input.is_action_pressed("ui_accept"):
+			# spawn bullet
+
+			# player is pushed backwards
+			var direction = Vector2(-sin(rotation), cos(rotation)).normalized()
+
+			velocity += direction * recoil_force
+
 			animated_sprite.play("FireBullet")
 		else:
 			animated_sprite.play("Idle")
+	move_and_slide();
