bf5f48d47c46791d381abe61b58037194c535f66
alanfogel <alanjfogel@gmail.com>
2024-09-27 18:35:38 -0600

    Added a 2nd sprite as a work around to allow for multipul animations to play at once, so we can see the shit fireing and rotating at the same time!

--- a2/player.gd
+++ a2/player.gd
@@ -14,16 +14,26 @@ func _process(_delta: float) -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	var animated_sprite = $AnimatedSprite2D
+	var rotate_sprite = $RotateSprite
+	var fire_sprite = $FireSprite
+	var camera = get_viewport().get_camera_2d()
+	var viewport_size = get_viewport_rect().size
 
-	if animated_sprite != null:
+	if rotate_sprite != null and fire_sprite != null:
+		var is_rotating = false
+		var is_fireing = false
+
 		if Input.is_action_pressed("ui_left"):
 			rotate(-turn_speed * delta)
-			animated_sprite.play("RotateLeft")
+			rotate_sprite.play("RotateLeft")
+			is_rotating = true
 		elif Input.is_action_pressed("ui_right"):
 			rotate(turn_speed * delta)
-			animated_sprite.play("RotateRight")
-		elif Input.is_action_pressed("ui_accept"):
+			rotate_sprite.play("RotateRight")
+			is_rotating = true
+
+
+		if Input.is_action_pressed("ui_accept"):
 			# spawn bullet
 
 			# player is pushed backwards
@@ -31,7 +41,29 @@ func _physics_process(delta: float) -> void:
 
 			velocity += direction * recoil_force
 
-			animated_sprite.play("FireBullet")
-		else:
-			animated_sprite.play("Idle")
+			fire_sprite.play("FireBullet")
+			is_fireing = true
+
+		# Play Idle animation only if not rotating or firing
+		if not is_rotating and not is_fireing:
+			rotate_sprite.play("Idle")
+			fire_sprite.play("Idle")
+
+	
+	
+	# Move the player
 	move_and_slide();
+
+	# Teleporting the player to the other side of the screen
+	var camera_position = camera.global_position
+	var half_viewport_size = viewport_size / 2
+
+	if position.x < camera_position.x - half_viewport_size.x:
+		position.x = camera_position.x + half_viewport_size.x
+	elif position.x > camera_position.x + half_viewport_size.x:
+		position.x = camera_position.x - half_viewport_size.x
+	
+	if position.y < camera_position.y - half_viewport_size.y:
+		position.y = camera_position.y + half_viewport_size.y
+	elif position.y > camera_position.y + half_viewport_size.y:
+		position.y = camera_position.y - half_viewport_size.y
