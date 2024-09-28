35df0813ef5800759770c5afbdb7e84d7ccaecb3
alanfogel <alanjfogel@gmail.com>
2024-09-27 19:00:41 -0600

    Added 3rd sprite to handle warping animations, added player warping when moved outside of camera view to opposite side while retaining velocity. Fixed conintued animations playing even when button nolonger pressed

--- a2/player.gd
+++ a2/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 @export var turn_speed := 3.0
 @export var recoil_force := 1.0
+@export var warping_duration := 1.0
+
+var warping_timer := 0.0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +19,7 @@ func _process(_delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	var rotate_sprite = $RotateSprite
 	var fire_sprite = $FireSprite
+	var warp_sprite = $WarpSprite
 	var camera = get_viewport().get_camera_2d()
 	var viewport_size = get_viewport_rect().size
 
@@ -45,11 +49,11 @@ func _physics_process(delta: float) -> void:
 			is_fireing = true
 
 		# Play Idle animation only if not rotating or firing
-		if not is_rotating and not is_fireing:
+		if not is_rotating:
 			rotate_sprite.play("Idle")
+		if not is_fireing:
 			fire_sprite.play("Idle")
 
-	
 	
 	# Move the player
 	move_and_slide();
@@ -57,13 +61,29 @@ func _physics_process(delta: float) -> void:
 	# Teleporting the player to the other side of the screen
 	var camera_position = camera.global_position
 	var half_viewport_size = viewport_size / 2
+	var teleported = false
 
 	if position.x < camera_position.x - half_viewport_size.x:
 		position.x = camera_position.x + half_viewport_size.x
+		teleported = true
 	elif position.x > camera_position.x + half_viewport_size.x:
 		position.x = camera_position.x - half_viewport_size.x
+		teleported = true
 	
 	if position.y < camera_position.y - half_viewport_size.y:
 		position.y = camera_position.y + half_viewport_size.y
+		teleported = true
 	elif position.y > camera_position.y + half_viewport_size.y:
 		position.y = camera_position.y - half_viewport_size.y
+		teleported = true
+
+	# Play warping animation if teleported
+	if teleported:
+		warp_sprite.play("Warping")
+		warping_timer = warping_duration
+
+	# Update warping timer
+	if warping_timer > 0:
+		warping_timer -= delta
+		if warping_timer <= 0:
+			warp_sprite.play("Idle")  # Optionally switch to an idle animation
